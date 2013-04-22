5e0b5f0a2bacfd61de254fd7fe4c332e25a4cd87
Fabrizio Codello <fabryz@gmail.com>
2013-04-22 22:20:21 +0200

    Indenting with spaces

--- public/js/backtime.js
+++ public/js/backtime.js
@@ -1,147 +1,147 @@
 $(document).ready(function() {
-	var Debug = {
+    var Debug = {
 
-		log: function (msg) {
-			console.log(new Date().toJSON() +": "+ msg);
-		},
+        log: function (msg) {
+            console.log(new Date().toJSON() +": "+ msg);
+        },
 
-		toggle: function(speed) {
-			speed = speed || 'fast';
-			defaultDebug.slideToggle(speed);
-		}
-	};
+        toggle: function(speed) {
+            speed = speed || 'fast';
+            defaultDebug.slideToggle(speed);
+        }
+    };
 
-	function init() {
-		Debug.log("Connecting...");
+    function init() {
+        Debug.log("Connecting...");
 
-		$('input').focus(function() {
-			isTyping = true;
-		});
+        $('input').focus(function() {
+            isTyping = true;
+        });
 
-		$('input').blur(function() {
-			isTyping = false;
-		});
+        $('input').blur(function() {
+            isTyping = false;
+        });
 
-		limit.on('change', function() {
-			tweets.html('');
-			showMaxTweets = limit.val();
-		});
+        limit.on('change', function() {
+            tweets.html('');
+            showMaxTweets = limit.val();
+        });
 
-		$(document).keyup(function(e) {
+        $(document).keyup(function(e) {
 
-			if (!isTyping) {
-				switch (e.keyCode) {
-					case 32: // space
-							tweets.html('');
-						break;
-					case 220: // backslash
-							Debug.toggle();
-						break;
-				}
-			} else { // input field has the focus
-				switch (e.keyCode) {
-					case 13: // enter
-							socket.emit('search', { search: search.val() });
-							search.val('');
-							tweets.html('');
-							maxSpeed.html(0);
-						break;
-				}
-			}
+            if (!isTyping) {
+                switch (e.keyCode) {
+                    case 32: // space
+                            tweets.html('');
+                        break;
+                    case 220: // backslash
+                            Debug.toggle();
+                        break;
+                }
+            } else { // input field has the focus
+                switch (e.keyCode) {
+                    case 13: // enter
+                            socket.emit('search', { search: search.val() });
+                            search.val('');
+                            tweets.html('');
+                            maxSpeed.html(0);
+                        break;
+                }
+            }
 
-		});
+        });
 
-		calcMaxPerSecond();
-		showMaxTweets = limit.val();
-	}
+        calcMaxPerSecond();
+        showMaxTweets = limit.val();
+    }
 
-	function calcMaxPerSecond() {
-		maxPerSecondInterval = setInterval(function() {
-			speed.html(tweetsAmount);
+    function calcMaxPerSecond() {
+        maxPerSecondInterval = setInterval(function() {
+            speed.html(tweetsAmount);
 
-			if (maxTweetsAmount < tweetsAmount) {
-				maxTweetsAmount = tweetsAmount;
-			}
+            if (maxTweetsAmount < tweetsAmount) {
+                maxTweetsAmount = tweetsAmount;
+            }
 
-			maxSpeed.html(maxTweetsAmount);
+            maxSpeed.html(maxTweetsAmount);
 
-			tweetsAmount = 0;
-		}, 1000);
-	}
+            tweetsAmount = 0;
+        }, 1000);
+    }
 
-	/*
-	* Main
-	*/
+    /*
+    * Main
+    */
 
-	var socket = new io.connect(window.location.href);
-	
-	var tweets = $("#tweets ul"),
-		defaultDebug = $("#stats"),
-		search = $("input#search"),
-		speed = $("#speed"),
-		maxSpeed = $("#maxSpeed"),
-		query = $('span#query'),
-		limit = $('select#limit'),
-		maxPerSecondInterval = null,
-		tweetsAmount = 0,
-		maxTweetsAmount = 0,
-		isTyping = false,
-		showMaxTweets = 0;
+    var socket = new io.connect(window.location.href);
 
-	init();
+    var tweets = $("#tweets ul"),
+        defaultDebug = $("#stats"),
+        search = $("input#search"),
+        speed = $("#speed"),
+        maxSpeed = $("#maxSpeed"),
+        query = $('span#query'),
+        limit = $('select#limit'),
+        maxPerSecondInterval = null,
+        tweetsAmount = 0,
+        maxTweetsAmount = 0,
+        isTyping = false,
+        showMaxTweets = 0;
 
-	/*
-	* Socket stuff
-	*/
+    init();
 
-	function convertURLs(str){
-		var regex = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/g;
-		return str.replace(regex, "<a href='$1' title='Open this link in a new tab' target='_blank'>$1</a>");
-	}
+    /*
+    * Socket stuff
+    */
 
-	function strdecode(data) {
-		return JSON.parse(decodeURIComponent(escape(data)));
-	}
+    function convertURLs(str){
+        var regex = /(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/g;
+        return str.replace(regex, "<a href='$1' title='Open this link in a new tab' target='_blank'>$1</a>");
+    }
 
-	socket.on('connect', function() {
-		Debug.log("Connected.");
-	});
-			
-	socket.on('disconnect', function() {
-		Debug.log("Disconnected.");
-		clearInterval(maxPerSecondInterval);
-	});
-		
-	socket.on('tot', function(data) {
-		Debug.log("Current viewers: "+ data.tot);
-	});
+    function strdecode(data) {
+        return JSON.parse(decodeURIComponent(escape(data)));
+    }
 
-	socket.on('filters', function(data) {
-		query.html(data.value);
-		Debug.log("Param: "+ data.param +", value: "+ data.value);
-	});
+    socket.on('connect', function() {
+        Debug.log("Connected.");
+    });
 
-	socket.on('tweet', function(tweet) {
-		tweet = strdecode(tweet);
+    socket.on('disconnect', function() {
+        Debug.log("Disconnected.");
+        clearInterval(maxPerSecondInterval);
+    });
 
-		if (showMaxTweets != 0) { // != infinite list
-			if (tweets.find("li").length + 1 > showMaxTweets ) {
-				tweets.find("li:last").remove();
-			}
-		}
-		// <img id="avatar" src="'+ tweet.user.profile_image_url +'" alt="" width="48" height="48" /> 
+    socket.on('tot', function(data) {
+        Debug.log("Current viewers: "+ data.tot);
+    });
+
+    socket.on('filters', function(data) {
+        query.html(data.value);
+        Debug.log("Param: "+ data.param +", value: "+ data.value);
+    });
+
+    socket.on('tweet', function(tweet) {
+        tweet = strdecode(tweet);
+
+        if (showMaxTweets !== 0) { // != infinite list
+            if (tweets.find("li").length + 1 > showMaxTweets ) {
+                tweets.find("li:last").remove();
+            }
+        }
+        // tweets.prepend('<li><img id="avatar" src="'+ tweet.user.profile_image_url +'" alt="" width="48" height="48" /> '+ tweet.created_at +' <strong><a href="http://www.twitter.com/'+ tweet.user.screen_name +'" title="Open profile" target="_blank">'+ tweet.user.screen_name +'</a></strong>: '+ convertURLs(tweet.text) +' (Retweets: '+ tweet.retweet_count +')</li>');
         tweets.prepend('<li>'+ tweet.created_at +' <strong><a href="http://www.twitter.com/'+ tweet.user.screen_name +'" title="Open profile" target="_blank">'+ tweet.user.screen_name +'</a></strong>: '+ convertURLs(tweet.text) +' (Retweets: '+ tweet.retweet_count +')</li>');
 
-		tweetsAmount++;
-	});
+        tweetsAmount++;
+    });
 
-	// socket.on('oldtweets', function(feed) {
-	// 	feed = strdecode(feed);
+    // socket.on('oldtweets', function(feed) {
+    //  feed = strdecode(feed);
 
-	// 	tweets.append("<li> * old tweets *</li>");
-	// 	feed.results.forEach(function(tweet, index) {
-	// 		// console.log(index, tweet.created_at, tweet.text);
-	// 		tweets.append("<li>"+ tweet.created_at +" "+ tweet.from_user +": "+ convertURLs(tweet.text) +" (Retweets: "+ (typeof tweet.metadata.recent_retweets == "undefined" ? "0" : tweet.metadata.recent_retweets) +")</li>");
-	// 	});
-	// });
+    //  tweets.append("<li> * old tweets *</li>");
+    //  feed.results.forEach(function(tweet, index) {
+    //      // console.log(index, tweet.created_at, tweet.text);
+    //      tweets.append("<li>"+ tweet.created_at +" "+ tweet.from_user +": "+ convertURLs(tweet.text) +" (Retweets: "+ (typeof tweet.metadata.recent_retweets == "undefined" ? "0" : tweet.metadata.recent_retweets) +")</li>");
+    //  });
+    // });
 });
